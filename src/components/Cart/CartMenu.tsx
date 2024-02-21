ee4f36199b6c2423845df84a93e1245fa20e5f1c
HeeJoon.Yang <yhj2151@gmail.com>
2024-02-22 00:15:48 +0900

    ✨ feat: 장바구니 상품 숫자 업데이트 추가

--- src/components/Cart/CartMenu.tsx
+++ src/components/Cart/CartMenu.tsx
@@ -3,14 +3,25 @@ import {
   NavigationMenuLink,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
+import { useContext } from "react";
+import { CartContext } from "..//../context/CartContext";
 import CartContents from "./CartContents";
+import { ShoppingCart } from "lucide-react";
 
 function CartMenu() {
+  const { cartItems } = useContext(CartContext);
+
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+        <NavigationMenuLink
+          className={`${navigationMenuTriggerStyle()} relative`}
+        >
           장바구니
+          <ShoppingCart />
+          <div className="absolute top-0 right-0 flex items-center justify-center w-5 h-5 text-xs text-white bg-red-500 rounded-full">
+            {cartItems.length > 0 && <div>{cartItems.length}</div>}
+          </div>
         </NavigationMenuLink>
       </SheetTrigger>
       <CartContents />
